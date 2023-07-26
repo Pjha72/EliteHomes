93a35a64625e903ded46c020ec3616b9b95347b7
Pawan Jha <pjha76737@gmail.com>
2023-07-26 17:59:35 +0530

    Add places for rent and sale page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import EditListing from './pages/EditListing';
 import Listing from './pages/Listing';
+import Category from './pages/Category';
 
 
 function App() {
@@ -29,6 +30,7 @@ function App() {
           <Route path='/sign-out' element={<SignOut />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/offers' element={<Offers />} />
+          <Route path='/category/:categoryName' element={<Category />} />
           <Route path='/category/:categoryName/:listingId' element={<Listing />} />
           <Route path='/create-listing' element={<PrivateRoute />}>
             <Route path='/create-listing' element={<CreateListing />} />
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -4,12 +4,14 @@ import { toast } from 'react-toastify';
 import { db } from "../firebase";
 import Spinner from '../components/Spinner';
 import ListingItem from '../components/ListingItem';
+import { useParams } from 'react-router-dom';
 
-export default function Offers() {
+export default function Category() {
   // fetching the data of the offers section
   const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(true);
   const [lastfetchlisting, setLastfetchlisting] = useState(null);
+  const params = useParams();
 
   useEffect(()=>{
     async function fetchListing(){
@@ -17,7 +19,7 @@ export default function Offers() {
         // get refernce 
         const listingRef = collection(db,"listings");
         // write a query to fethc the data
-        const q = query(listingRef, where("offer","==",true), orderBy("timestamp","desc"), limit(8));
+        const q = query(listingRef, where("type","==",params.categoryName), orderBy("timestamp","desc"), limit(8));
         // Execute the query
         const querySnap = await getDocs(q);
         const lastVisible = querySnap.docs[querySnap.docs.length - 1];
@@ -37,7 +39,7 @@ export default function Offers() {
       }
     }
     fetchListing();
-  },[]);
+  },[params.categoryName]);
   
   // fetch more listings
   async function onFetchMoreListings(){
@@ -45,7 +47,7 @@ export default function Offers() {
       // get refernce 
       const listingRef = collection(db,"listings");
       // write a query to fethc the data
-      const q = query(listingRef, where("offer","==",true), orderBy("timestamp","desc"),
+      const q = query(listingRef, where("type","==",params.categoryName), orderBy("timestamp","desc"),
       startAfter(lastfetchlisting),
        limit(4));
       // Execute the query
@@ -69,7 +71,7 @@ export default function Offers() {
 
   return (
     <div className='max-w-6xl mx-auto px-3'> 
-      <h1 className='text-3xl text-center mt-6 font-bold mb-6'>Offers</h1>
+      <h1 className='text-3xl text-center mt-6 font-bold mb-6'>{params.categoryName === "rent" ? "Places for rent" : "Places for sale"}</h1>
       {loading ? (
         <Spinner />
       ): listings && listings.length > 0 ? (
@@ -90,7 +92,7 @@ export default function Offers() {
           )}
         </>
       ) : (
-        <p>There are no current offers</p>
+        <p>There are no current {params.categoryName === "rent" ? "places for rent" : "places for sale"}</p>
       )}
     </div>
   )
